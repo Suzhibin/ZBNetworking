d1689a3ded88015c092be9d0ae573d8db658b4f2
Suzhibin <szb2323@163.com>
2017-08-21 14:42:59 +0800

    代码优化

--- ZBNetworking/ZBRequestEngine.m
+++ ZBNetworking/ZBRequestEngine.m
@@ -16,25 +16,31 @@
     static ZBRequestEngine *sharedInstance = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        sharedInstance = [ZBRequestEngine manager];
+        sharedInstance = [[ZBRequestEngine alloc]init];
+    });
+    return sharedInstance;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
         //无条件地信任服务器端返回的证书。
-        sharedInstance.securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeNone];
-        sharedInstance.securityPolicy = [AFSecurityPolicy defaultPolicy];
-        sharedInstance.securityPolicy.allowInvalidCertificates = YES;
-        sharedInstance.securityPolicy.validatesDomainName = NO;
+        self.securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeNone];
+        self.securityPolicy = [AFSecurityPolicy defaultPolicy];
+        self.securityPolicy.allowInvalidCertificates = YES;
+        self.securityPolicy.validatesDomainName = NO;
         /*因为与缓存互通 服务器返回的数据 必须是二进制*/
-        sharedInstance.responseSerializer = [AFHTTPResponseSerializer serializer];
+        self.responseSerializer = [AFHTTPResponseSerializer serializer];
         
-        sharedInstance.operationQueue.maxConcurrentOperationCount = 5;
-        sharedInstance.requestSerializer.timeoutInterval = 15.f;
-        sharedInstance.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html",@"application/json",@"text/json", @"text/plain",@"text/javascript",nil];
+        self.operationQueue.maxConcurrentOperationCount = 5;
+        self.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html",@"application/json",@"text/json", @"text/plain",@"text/javascript",nil];
         [AFNetworkActivityIndicatorManager sharedManager].enabled = YES;
         //忽略系统缓存
         NSURLCache *sharedCache = [[NSURLCache alloc] initWithMemoryCapacity:0 diskCapacity:0 diskPath:nil];
         [NSURLCache setSharedURLCache:sharedCache];
         
-    });
-    return sharedInstance;
+    }
+    return self;
 }
 
 - (NSString *)cancelRequest:(NSString *)urlString{
