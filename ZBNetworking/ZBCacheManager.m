7d1ed1691599c342553e69a2875278a3b8a05d66
Andi <szb2323@163.com>
2020-08-03 12:54:15 +0800

    代码优化

--- ZBNetworking/ZBCacheManager.m
+++ ZBNetworking/ZBCacheManager.m
@@ -177,10 +177,8 @@ static const CGFloat unit = 1000.0;
     }
     if ([content isKindOfClass:[NSData class]]) {
         return  [(NSData *)content writeToFile:path atomically:YES];
-    }else if ([content isKindOfClass:[UIImage class]]) {
-        return [UIImageJPEGRepresentation((UIImage *)content,(CGFloat)0.9) writeToFile:path atomically:YES];
     }else {
-       // [NSException raise:@"非法的文件内容" format:@"文件类型%@异常。", NSStringFromClass([content class])];
+        NSLog(@"文件类型:%@,沙盒存储失败。",NSStringFromClass([content class]));
         return NO;
     }
     return NO;
